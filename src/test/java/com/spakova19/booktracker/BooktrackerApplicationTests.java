3ae08b77d5ff1941a64424e3a2146eed3b20e9a3
spakova19 <spakova20@gmail.com>
2026-07-14 22:14:51 +0200

    initialization of Spring Boot project

---
+++ src/test/java/com/spakova19/booktracker/BooktrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spakova19.booktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
